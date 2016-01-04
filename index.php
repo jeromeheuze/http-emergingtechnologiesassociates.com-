9038316650f1f755ee5b3eb1a60e62ea1abeb1d3
Jerome Heuze <jh@societysource.com>
2016-01-04 00:59:05 -0800

    updated hp

--- index.php
+++ index.php
@@ -17,32 +17,22 @@
                     src="/graphics/logo-ETA.png" alt="Emerging Technologies Associates logo"/></a></div>
         <div id="features">
             <div class="feat-notab">
-                <a href="ENERGY_STAR_Energy_Management_and_Strategy.php"><img src="/graphics/f1.png"
-                                                                              alt="ENERGY STAR® Energy Management and Strategy"/></a>
                 <a href="ENERGY_STAR_Energy_Management_and_Strategy.php"><h2>ENERGY STAR&reg; Energy Management and
                         Strategy</h2></a>
             </div>
             <div class="feat-notab">
-                <a href="Resource_Productivity_Enhancement_Strategies.php"><img src="/graphics/f2.png"
-                                                                                alt="Resource Productivity Enhancement Strategies"/></a>
                 <a href="Resource_Productivity_Enhancement_Strategies.php"><h2>Resource Productivity Enhancement
                         Strategies</h2></a>
             </div>
             <div class="feat-notab">
-                <a href="The_Financial_Strategies_of_Energy_Efficiency.php"><img src="/graphics/f3.png"
-                                                                                 alt="The Financial Strategies of Energy Efficiency"/></a>
                 <a href="The_Financial_Strategies_of_Energy_Efficiency.php"><h2>The Financial Strategies of Energy
                         Efficiency</h2></a>
             </div>
             <div class="feat-notab">
-                <a href="Strategic_Management_and_Knowledge_Transfer.php"><img src="/graphics/f4.png"
-                                                                               alt="Strategic Management and Knowledge Transfer"/></a>
                 <a href="Strategic_Management_and_Knowledge_Transfer.php"><h2>Strategic Management and Knowledge
                         Transfer</h2></a>
             </div>
             <div class="feat-notab">
-                <a href="Emerging_Technology_Market_Development.php"><img src="/graphics/f5.png"
-                                                                          alt="Emerging Technology Market Development"/></a>
                 <a href="Emerging_Technology_Market_Development.php"><h2>Emerging Technology Market Development</h2></a>
             </div>
         </div>
@@ -71,9 +61,6 @@
             </div>
 
             <div id="up-seminars">
-                <img src="/graphics/up-seminars-feat.jpg" alt="USA Today - Kerstin Joensson, AP"
-                     style="margin-bottom: 40px;"/>
-
                 <h2 class="th2">Current Seminars and Events</h2>
                 <ul id="seminars">
                     <li><span>Jul. 7-9, 2015</span> <a href="/seminars/">The 3rd International Symposium on Energy Challenges and Mechanics</a></li>
